a4e92b1d30953c1aa0d909f5051b4477be2ac437
Igor Malyugin <i.malyugin00@gmail.com>
2022-05-08 18:36:34 +0300

    Modified main page - new Card component, mock data added

---
+++ src/data/dataHotels.ts
@@ -0,0 +1,60 @@
+import type { HotelList } from '../types/hotel';
+
+const hotels: HotelList = [
+  {
+    src: 'www.google.com',
+    name: 'Super Hotel',
+    type: 'Apartments',
+    imageSrc: 'img/apartment-02.jpg',
+    price: 50,
+    rating: 10,
+    id: 1,
+  },
+  {
+    src: 'www.google.com',
+    name: 'Ultra Hotel',
+    type: 'Apartments',
+    imageSrc: 'img/apartment-01.jpg',
+    price: 550,
+    rating: 60,
+    id: 2,
+  },
+  {
+    src: 'www.google.com',
+    name: 'default hotel',
+    type: 'Apartments',
+    imageSrc: 'img/apartment-03.jpg',
+    price: 1488,
+    rating: 30,
+    id: 3,
+  },
+  {
+    src: 'www.google.com',
+    name: 'Super mega ultra duper aps',
+    type: 'Apartments',
+    imageSrc: 'img/apartment-04.jpg',
+    price: 125125,
+    rating: 90,
+    id: 4,
+  },
+  {
+    src: 'www.google.com',
+    name: 'Ebat house',
+    type: 'House',
+    imageSrc: 'img/apartment-05.jpg',
+    price: 5120,
+    rating: 80,
+    id: 5,
+  },
+  {
+    src: 'www.google.com',
+    name: 'Super Hotel',
+    type: 'Apartments',
+    imageSrc: 'img/apartment-02.jpg',
+    price: 880,
+    rating: 50,
+    id: 6,
+  },
+];
+
+export default hotels;
---
+++ src/types/hotel.ts
@@ -0,0 +1,11 @@
+export type Hotel = {
+    src: string;
+    name: string;
+    type: string;
+    imageSrc: string;
+    price: number;
+    rating: number;
+    id: number,
+}
+
+export type HotelList = Hotel[];
